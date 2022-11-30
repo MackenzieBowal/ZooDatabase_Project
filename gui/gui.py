52df1cf9995fe3a71803130b4d459f5b67e6f0db
MackenzieBowal <mackenzie.bowal@gmail.com>
2022-11-29 17:22:45 -0700

    Added species viewing!!!

--- gui/gui.py
+++ gui/gui.py
@@ -1,32 +1,55 @@
 from tkinter import *
+from visitor import handleVisitorPage
+
 
 def login():
     loginPage.place(relwidth=1,relheight=1)
 
-window = Tk() #Create the main window
-window.title("Our Zoo Database System") #Set the title of the window
-window.geometry("1024x768") #Set the size of the window
+def visitorClick():
+    handleVisitorPage(window, welcomePage)
 
-welcomePage = Frame(window)
-loginPage = Frame(window)
+def mainMenu():
+    welcomePage.place(relwidth=1,relheight=1)
 
-welcomeMessage = Label(welcomePage,text="Welcome to the Zoo!\nPlease log in to continue.")
-employeeButton = Button(welcomePage,text="Employee",command=login)
-visitorButton = Button(welcomePage,text="Visitor")
+def initiate():
+    global window
+    window = Tk()
+    window.title("Our Zoo Database System") # Set the title of the window
+    window.geometry("1024x768") # Set the size of the window
 
-welcomeMessage.grid(row=0,column=0,columnspan=2,sticky=N+S+W+E,padx=5)
-employeeButton.grid(row=1,column=0,sticky=N+S+W+E,padx=10,pady=5)
-visitorButton.grid(row=1,column=1,sticky=N+S+W+E,padx=10,pady=5)
+    # make welcome page
+    global welcomePage 
+    welcomePage = Frame(window)
 
-#Configure the grid elements to expand to fill extra space
-welcomePage.rowconfigure(0,weight=1)
-welcomePage.rowconfigure(1,weight=1)
-welcomePage.columnconfigure(0,weight=1)
-welcomePage.columnconfigure(1,weight=1)
+    welcomeMessage = Label(welcomePage,text="Welcome to the Zoo!\nPlease log in to continue.")
+    employeeButton = Button(welcomePage,text="Employee",command=login)
+    visitorButton = Button(welcomePage,text="Visitor", command=visitorClick)
 
-testText = Label(loginPage,text="Test text")
-testText.grid(row=0,column=0,sticky=N+S+W+E)
+    welcomeMessage.grid(row=0,column=0,columnspan=2,sticky=N+S+W+E,padx=5)
+    employeeButton.grid(row=1,column=0,sticky=N+S+W+E,padx=10,pady=5)
+    visitorButton.grid(row=1,column=1,sticky=N+S+W+E,padx=10,pady=5)
 
-welcomePage.place(relwidth=1,relheight=1)
+    #Configure the grid elements to expand to fill extra space
+    welcomePage.rowconfigure(0,weight=1)
+    welcomePage.rowconfigure(1,weight=1)
+    welcomePage.columnconfigure(0,weight=1)
+    welcomePage.columnconfigure(1,weight=1)
 
-window.mainloop() #An infinite loop, runs until we close the window
+
+    # make employee login page
+    global loginPage
+    loginPage = Frame(window)
+    testText = Label(loginPage,text="Test text")
+    testText.grid(row=0,column=0,sticky=N+S+W+E)
+
+
+    welcomePage.place(relwidth=1,relheight=1)
+
+    window.mainloop() #An infinite loop, runs until we close the window
+
+initiate()
+
+
+
+
+
