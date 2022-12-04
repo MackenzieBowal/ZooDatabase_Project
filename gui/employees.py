dbb7b07e22c0862e19b9c2b2b6ec512f5672e2cf
MackenzieBowal <mackenzie.bowal@gmail.com>
2022-12-04 00:40:06 -0700

    Now employees works (I think)

--- gui/employees.py
+++ gui/employees.py
@@ -17,19 +17,6 @@ mycursor = mydb.cursor()
 
 def show_employee(event):
 
-    """
-    global worksAtStoreLabel
-    worksAtStoreLabel = Label(employeesFrame, text="Works at store: ")
-    worksAtStoreLabel.grid(row=8)
-
-
-
-    #Erase the details of any previous zookeeper/manager that are still on the screen
-    if previousEmployee == "storeEmp":
-        worksAtStoreLabel.destroy()
-
-    """
-
     employee = employeeSelection.get()
     employeeLabel.config(text="Employee ID: " + employee)
 
@@ -62,97 +49,6 @@ def show_employee(event):
     if result == "None":
         result = "N/A"
     startdateLabel.config(text="Start Date: " + result)
-
-    '''
-    mycursor.execute("SELECT * FROM Zookeeper WHERE EmployeeID = '%s'"%employee)
-    result = mycursor.fetchall()
-    if len(result) > 0: #Employee is a zookeeper
-        show_zookeeper(employee)
-    else:
-        mycursor.execute("SELECT * FROM Manager WHERE EmployeeID = '%s'"%employee)
-        result = mycursor.fetchall()
-        if len(result) > 0: #Employee is a manager
-            show_manager(employee)
-        else: #Employee has some other role
-            show_other(employee)
-    '''
-
-"""
-def show_zookeeper(employee):
-    global previousEmployee
-    global specializationLabel
-    global caresforLabel
-
-    #Erase the details of any previous zookeeper/manager that are still on the screen
-    if previousEmployee == "zookeeper":
-        specializationLabel.destroy()
-        caresforLabel.destroy()
-    elif previousEmployee == "manager":
-        previousroleLabel.destroy()
-        overlooksLabel.destroy()
-
-    previousEmployee = "zookeeper"
-
-    specializationLabel = Label(employeesFrame, text="Specialization: ")
-    specializationLabel.grid(column = 1, row = 2, pady=20, sticky=S+E+W)
-    caresforLabel = Label(employeesFrame, text="Cares for ")
-    caresforLabel.grid(column = 1, row = 3, pady=20, sticky=N+S+E+W)
-
-    mycursor.execute("SELECT Specialization FROM Zookeeper_specialization WHERE Zookeeper_EID = '%s'"%employee)
-    result = str(mycursor.fetchall()[0][0])
-    specializationLabel.config(text="Specialization: " + result)
-
-    mycursor.execute("SELECT Species_name FROM Cares_for WHERE Zookeeper_EID = '%s'"%employee)
-    result = mycursor.fetchall()
-    caresforString = "Cares for "
-    for species in result:
-        caresforString = caresforString + species[0] + " "
-    caresforLabel.config(text=caresforString)
-
-def show_manager(employee):
-    global previousEmployee
-    global previousroleLabel
-    global overlooksLabel
-
-    #Erase the details of any previous zookeeper/manager that are still on the screen
-    if previousEmployee == "zookeeper":
-        specializationLabel.destroy()
-        caresforLabel.destroy()
-    elif previousEmployee == "manager":
-        previousroleLabel.destroy()
-        overlooksLabel.destroy()
-
-    previousEmployee = "manager"
-
-    previousroleLabel = Label(employeesFrame, text="Previous Roles: ")
-    previousroleLabel.grid(column = 1, row = 2, pady=20, sticky=S+E+W)
-    overlooksLabel = Label(employeesFrame, text="Overlooks ")
-    overlooksLabel.grid(column = 1, row = 3, pady=20, sticky=N+S+E+W)
-
-    mycursor.execute("SELECT Previous_role FROM Manager_previousrole WHERE Manager_EID = '%s'"%employee)
-    result = mycursor.fetchall()
-    previousroleString = "Previous Roles: "
-    for role in result:
-        previousroleString = previousroleString + role[0] + " "
-    previousroleLabel.config(text=previousroleString)
-
-    mycursor.execute("SELECT FundraiserID FROM Overlooks WHERE Manager_EID = '%s'"%employee)
-    result = mycursor.fetchall()
-    overlooksString = "Overlooks fundraisers: "
-    for fundraiser in result:
-        overlooksString = overlooksString + fundraiser[0] + " "
-    overlooksLabel.config(text=overlooksString)
-"""
-"""
-def show_other(employee):
-    #Erase the details of any previous zookeeper/manager that are still on the screen
-    if previousEmployee == "zookeeper":
-        specializationLabel.destroy()
-        caresforLabel.destroy()
-    elif previousEmployee == "manager":
-        previousroleLabel.destroy()
-        overlooksLabel.destroy()
-"""
 
 #################################################
 
@@ -306,7 +202,6 @@ def addClick():
     phone = phoneBox.get()
     date = dateBox.get()
     role = roleBox.get()
-    #store = storeBox.get()
 
     try:
         mycursor.execute("INSERT INTO Employee \
@@ -321,7 +216,7 @@ def addClick():
         elif (role == 'Manager'):
                 mycursor.execute("INSERT INTO Manager VALUES ("+eid+")")
         elif (role == 'Store Employee'):
-                mycursor.execute("INSERT INTO Other_employee VALUES ("+eid+", 'NULL')")
+                mycursor.execute("INSERT INTO Other_employee VALUES ("+eid+", NULL)")
     except:
         showerror(title="Error",message="Invalid input. Please try again.")
     return
@@ -389,22 +284,6 @@ def addEmployee():
     roleLabel.grid(row=7,column=0,sticky=E,padx=5,pady=10)
     roleBox.grid(row=7,column=1,sticky=E+W,padx=5,pady=10)
 
-    '''
-    # Store
-    storeLabel = Label(employeesFrame,text="Store (If store employee): ")
-    global storeBox
-    store = ""
-    storeBox = Combobox(employeesFrame, width = 30, textvariable = role)
-    mycursor.execute("SELECT Store_name FROM Store")
-    sNames = []
-    for i in mycursor.fetchall():
-        sNames.append(i[0])
-    storeBox['values'] = sNames
-    storeBox['state'] = 'readonly'
-    storeLabel.grid(row=8,column=0,sticky=E,padx=5,pady=10)
-    storeBox.grid(row=8,column=1,sticky=E+W,padx=5,pady=10)
-    '''
-
     addButton = Button(employeesFrame, text="Add", command=addClick)
     addButton.grid(row=9,columnspan=2)
 
@@ -464,8 +343,3 @@ def set_employees_frame(sFrame, mid):
     phoneLabel.grid(column = 1, row = 6, pady=20, sticky=N+S+E+W)
     startdateLabel = Label(employeesFrame, text="Start Date: ")
     startdateLabel.grid(column = 1, row = 7, pady=20, sticky=N+S+E+W)
-
-    '''
-    global previousEmployee #Tracks whether a zookeeper or manager was previously selected
-    previousEmployee = ""
-    '''
